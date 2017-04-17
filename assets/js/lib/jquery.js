4e8d13c7135b1bfc88aebb1163fcb2242ae4a4bb
Vonfry <vonfry314@icloud.com>
2017-04-17 18:01:51 +0800

    jquery requirejs change

--- assets/js/lib/jquery.js
+++ assets/js/lib/jquery.js
@@ -1,4 +1,4 @@
-define(['jquery'], function($) {
+define(['https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'], function($) {
     $ = $ ? $ : jQuery;
     return $.noConflict(true);
 });
--- assets/js/requireConfig.js
+++ assets/js/requireConfig.js
@@ -1,6 +1,5 @@
 require.config({
     baseUrl: siteURL + '/assets/js',
     paths: {
-        jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min'
     }
 });
